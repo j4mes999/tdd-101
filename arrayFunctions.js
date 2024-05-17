b5f6077eada74b2c6def80eae0fcf148cc202fce
Luis J. Gonzalez <jamesqc999@gmail.com>
2024-05-17 07:17:31 -0500

    Add array analysis implementation with UTs

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -1,12 +1,25 @@
 function analyzeArray(array){
-    return {average: 3,
-        min: 1,
-        max: 5,
+    return {average: average(array),
+        min: minimum(array),
+        max: maximum(array),
         length: array.length
     };
 }
 
+function average(array){
+    return roundNumber(array.reduce((a, b) => a + b) / array.length);
+}
 
+function roundNumber(number){
+    return Math.round( number * 1e2) / 1e2;
+}
 
+function minimum(array){
+    return Math.min(...array);
+}
+
+function maximum(array){
+    return Math.max(...array);
+}
 
 export default analyzeArray;
--- arrayFunctions.test.js
+++ arrayFunctions.test.js
@@ -5,5 +5,9 @@ test('Analysis of an array', () => {
 });
 
 test('Analysis of a more complex array', () => {
-  expect(analyzeArray([2,4,66,77,99,123]).toEqual())
+  expect(analyzeArray([2,4,66,77,99,123])).toEqual({average: 61.83, min: 2, max: 123, length: 6})
+});
+
+test('Analysis of an array ODIN example', () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({average: 4, min: 1, max: 8, length: 6});
 });
